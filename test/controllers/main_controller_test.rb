e828d87d19d5f9d270badb80d4565261b4401e6d
TikhampornSky <tikhamporntan@gmail.com>
2022-12-14 10:19:17 +0700

    Add more test in MAIN

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -12,4 +12,107 @@ class MainControllerTest < ActionDispatch::IntegrationTest
     get "/main"
     assert_response :success
   end
+
+  def admin_login
+    @email = '0@gmail.com'
+    @password = "0"
+    get '/login/create', params: { email: @email, password: @password }
+  end
+
+  def seller_login
+    @email = '1@gmail.com'
+    @password = "1"
+    get '/login/create', params: { email: @email, password: @password }
+  end
+
+  def buyer_login
+    @email = '2@gmail.com'
+    @password = "2"
+    get '/login/create', params: { email: @email, password: @password }
+  end
+
+  test "reach /proflie" do
+    admin_login()
+    get '/profile'
+    assert_response :success
+
+    seller_login()
+    get '/profile'
+    assert_response :success
+
+    buyer_login()
+    get '/profile'
+    assert_response :success
+  end
+
+  test "reach /my_market" do
+    admin_login()
+    get '/my_market'
+    assert_response :success
+
+    seller_login()
+    get '/my_market'
+    assert_redirected_to "/permission"
+
+    buyer_login()
+    get '/my_market'
+    assert_response :success
+  end
+
+  test "reach /purchase_history" do
+    admin_login()
+    get '/purchase_history'
+    assert_response :success
+
+    seller_login()
+    get '/purchase_history'
+    assert_redirected_to "/permission"
+
+    buyer_login()
+    get '/purchase_history'
+    assert_response :success
+  end
+
+  test "reach /sale_history" do
+    admin_login()
+    get '/sale_history'
+    assert_response :success
+
+    seller_login()
+    get '/sale_history'
+    assert_response :success
+
+    buyer_login()
+    get '/sale_history'
+    assert_redirected_to "/permission"
+  end
+
+  test "reach /my_inventory" do
+    admin_login()
+    get '/my_inventory'
+    assert_response :success
+
+    seller_login()
+    get '/my_inventory'
+    assert_response :success
+
+    buyer_login()
+    get '/my_inventory'
+    assert_redirected_to "/permission"
+  end
+
+  test "reach /top_seller" do
+    admin_login()
+    get '/top_seller'
+    assert_response :success
+
+    seller_login()
+    get '/top_seller'
+    assert_response :success
+
+    buyer_login()
+    get '/top_seller'
+    assert_redirected_to "/permission"
+  end
+
 end
